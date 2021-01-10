42fcf7c8eb709f3a64aeba1021a7b89902610e65
Sujinthorn <sujinthorn.keng@mail.kmutt.ac.th>
2021-01-11 01:33:33 +0700

    update backend

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ mongoose
     { useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || gethired-api.herokuapp.com);
   })
   .catch((err) => {
     console.log(err);
--- backend/models/job.js
+++ backend/models/job.js
@@ -9,9 +9,7 @@ const jobSchema = new Schema({
   companyName: { type: String, required: false },
   wage: { type: Number, required: true },
   expDate: { type: Date, required: true },
-
   categories: [{ type: String, required: true }],
-  
   companyAddress: { type: String, required: false },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   applier: [{ type: mongoose.Types.ObjectId, required: false, ref: "User" }],
